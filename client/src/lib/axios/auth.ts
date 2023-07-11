a1f8078fcba117b2fac0ab4eb3cf2c1bb4da33d6
Matthew Ivanov <matthewiva13@gmail.com>
2023-07-11 13:27:48 +0300

    login and signup client

---
+++ client/src/lib/axios/auth.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosResponse } from 'axios'
+import type { LoginResponse, SignupResponse, UserLoginCredentials, UserSignupCredentials } from '../../types/auth'
+import type { ApiError } from '../../types/api'
+
+const signupUser = async (userCredentials: UserSignupCredentials) => {
+  try {
+    const response: AxiosResponse<SignupResponse> = await axios.post('/api/users/signup', userCredentials, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+    return response.data
+  } catch (err) {
+    const data: ApiError = {
+      error: { message: 'Unexpected error has occurred', status: 'Internal Server Error', statusCode: 500 },
+    }
+    if (axios.isAxiosError(err)) {
+      return (err.response?.data as ApiError) || data
+    }
+    return data
+  }
+}
+
+const loginUser = async (userCredentials: UserLoginCredentials) => {
+  try {
+    const response: AxiosResponse<LoginResponse | ApiError> = await axios.post('/api/users/login', userCredentials, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+    return response.data
+  } catch (err) {
+    const data: ApiError = {
+      error: { message: 'Unexpected error has occurred', status: 'Internal Server Error', statusCode: 500 },
+    }
+    if (axios.isAxiosError(err)) {
+      return (err.response?.data as ApiError) || data
+    }
+    return data
+  }
+}
+
+const auth = { signupUser, loginUser }
+
+export default auth
