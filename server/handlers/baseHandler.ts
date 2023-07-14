0d8c937e70dde18d797dfdf6257036cabf7f3fb5
Matthew Ivanov <matthewiva13@gmail.com>
2023-07-14 19:49:47 +0300

    minor fixes + note middleware

--- server/handlers/baseHandler.ts
+++ server/handlers/baseHandler.ts
@@ -6,93 +6,81 @@ import { User } from '../models/User'
 import { Note } from '../models/Note'
 import APIFeatures from '../utils/APIFeatures'
 
-type Models = User | Note
+const getAll = (Model: Model<any>) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const features = new APIFeatures(Model.find(), req.query).sort().paginate().limitFields()
+    const docs = await features.query
 
-const getAll =
-  <T extends Models>(Model: Model<T>) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const features = new APIFeatures(Model.find(), req.query).sort().paginate().limitFields()
-      const docs = await features.query
-
-      res.status(200).json({
-        status: 'Success',
-        results: docs.length,
-        documents: docs,
-      })
-    } catch (err) {
-      next(err)
-    }
+    res.status(200).json({
+      status: 'Success',
+      results: docs.length,
+      documents: docs,
+    })
+  } catch (err) {
+    next(err)
   }
+}
 
-const getOne =
-  <T extends Models>(Model: Model<T>) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const doc = await Model.findById(req.params.id)
+const getOne = (Model: Model<any>) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const doc = await Model.findById(req.params.id)
 
-      if (!doc) {
-        return next(new AppError('No document found with that ID', 404, 'Bad Request'))
-      }
-
-      res.status(200).json({
-        status: 'Success',
-        document: doc,
-      })
-    } catch (err) {
-      next(err)
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404, 'Bad Request'))
     }
+
+    res.status(200).json({
+      status: 'Success',
+      document: doc,
+    })
+  } catch (err) {
+    next(err)
   }
+}
 
-const deleteOne =
-  <T extends Models>(Model: Model<T>) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const doc = await Model.findByIdAndDelete(req.params.id)
-      if (!doc) {
-        return next(new AppError('No document found with that ID', 404, 'Bad Request'))
-      }
-
-      res.status(204).json({
-        status: 'Success',
-      })
-    } catch (err) {
-      next(err)
+const deleteOne = (Model: Model<any>) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const doc = await Model.findByIdAndDelete(req.params.id)
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404, 'Bad Request'))
     }
+
+    res.status(204).json({
+      status: 'Success',
+    })
+  } catch (err) {
+    next(err)
   }
+}
 
-const updateOne =
-  <T extends Models>(Model: Model<T>) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const doc = await Model.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+const updateOne = (Model: Model<any>) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
-      if (!doc) {
-        return next(new AppError('No document found with that ID', 404, 'Bad Request'))
-      }
-
-      res.status(200).json({
-        status: 'Success',
-        patchedDocument: doc,
-      })
-    } catch (err) {
-      next(err)
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404, 'Bad Request'))
     }
-  }
 
-const createOne =
-  <T extends Models>(Model: Model<T>) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const doc = await Model.create(req.body)
-
-      res.status(201).json({
-        status: 'Success',
-        createdDocument: doc,
-      })
-    } catch (err) {
-      next(err)
-    }
+    res.status(200).json({
+      status: 'Success',
+      patchedDocument: doc,
+    })
+  } catch (err) {
+    next(err)
   }
+}
+
+const createOne = (Model: Model<any>) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const doc = await Model.create(req.body)
+
+    res.status(201).json({
+      status: 'Success',
+      createdDocument: doc,
+    })
+  } catch (err) {
+    next(err)
+  }
+}
 
 export default { getAll, getOne, deleteOne, updateOne, createOne }
